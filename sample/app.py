0a444d49849a9008dfc2d5654b9f1e9a22f37258
Amirul Islam <amirulislam@Admins-MacBook-Pro.local>
2021-10-29 03:02:49 +0600

    sample app added

---
+++ sample/app.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    active: Optional[bool] = True
+
+
+@app.get('/')
+def main():
+    return {
+        "message": "Hello World!"
+    }
+
+
+@app.get('/items/{id}')
+def get_items(id: int, q: Optional[str] = None):
+    return {
+        "id": id,
+        "q": q
+    }
+
+
+@app.put('/items/{id}')
+def update_item(id: int, item: Item):
+    return {
+        'id': id,
+        'item name': item.name,
+        'item price': item.price,
+        'active': item.active
+    }
